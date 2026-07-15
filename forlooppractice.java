46dcc5c45cd94c0008a8611f35c8578bccd404d7
aryaman <aryamangupta55@gmail.com>
2026-07-15 18:46:33 +0530

    9 problems solved of loops

--- forlooppractice.java
+++ forlooppractice.java
@@ -1,14 +1,135 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class forlooppractice {
+// public class forlooppractice {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner (System.in);
+//         int n = sc.nextInt();
+
+//         for (int i = 1; i <= n; i++)
+//         {
+//             System.out.println(i);
+//         }
+//     sc.close();
+// }
+// }
+
+// import java.util.Scanner ;    // print no 1 to 10
+// public class forlooppractice{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+        
+//         for(int i = 1; i <= 10; i++)
+//         {
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+
+// import java.util.Scanner ;    // print no 10 to 1 in reverse ordfer
+// public class forlooppractice{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+        
+//         for(int i = 10 ; i >= 1; i--)
+//         {
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+
+// import java.util.Scanner ;    // print all even numbers 1 to 100
+// public class forlooppractice{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+        
+//         for(int i = 2 ; i <= 100; i+=2)
+//         {
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+
+// import java.util.Scanner ;    // print all odd numbers 1 to 100
+// public class forlooppractice{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+        
+//         for(int i = 1 ; i <= 100; i+=2)
+//         {
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+// import java.util.Scanner;         //print table of n
+// public class forlooppractice{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner (System.in);
+//         System.out.println("enter number");
+//         int n = sc.nextInt();
+
+//         for(int i = 1; i <= 10; i++)
+//         {
+//             System.out.println(n +"*"+ i +"="+n*i);
+//         }
+//     }
+// }
+
+// import java.util.Scanner;          // factorial of givrn number
+// public class forlooppractice{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner (System.in);
+//         System.out.println("enter the number");
+//         int n = sc.nextInt();
+
+//     int fact = 1;
+
+//     for (int i = 1; i <= n; i++)
+//     {
+//         fact = fact * i;
+//         System.out.println(fact);
+//     }
+//     }
+// }
+
+// import java.util.Scanner ;       // fcatorial of evry number 1 to n
+// public class forlooppractice{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner (System.in);
+//         System.out.println("entr the number");
+//         int n = sc.nextInt();
+
+//         int fact = 1;
+
+//         for(int i = 1; i<= n; i++)
+//         {
+//             fact = fact * i;
+//             System.out.println(i +"* ="+fact);
+//         }
+//     }
+// }
+
+import java.util.Scanner;
+public class forlooppractice{
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         int n = sc.nextInt();
 
-        for (int i = 1; i <= n; i++)
+        boolean factorFound = false;
+
+        for(int i = 2; i <= n-1; i++)
         {
-            System.out.println(i);
+            if ( n % i == 0)
+            {
+                System.out.println("not prime");
+                factorFound = true;
+                break;
+            }
         }
-    sc.close();
-}
+        if ( factorFound = true) System.out.println("not");
+        else if (factorFound = false) System.out.println("yes");
+    }
 }
