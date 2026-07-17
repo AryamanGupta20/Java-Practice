a881dbb8569560fdde0af0b98c8fc959ad03461d
aryaman <aryamangupta55@gmail.com>
2026-07-17 17:16:26 +0530

    Loops problems

--- loops.java
+++ loops.java
@@ -200,30 +200,16 @@
 //         }
 //     }
 // }
+
 import java.util.Scanner;
-
-public class loops {
-
+public class loops{
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner (System.in);
-
-        System.out.println("Enter the number of terms:");
+        Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        int num1 = 0;
-        int num2 = 1;
-
-        for (int i = 0; i < n; i++) {
-
-            System.out.println(num1);
-
-            int next = num1 + num2;
-
-            num1 = num2;
-            num2 = next;
+        for(int i = 1; i <= n; i++)
+        {
+            System.out.println(i);
         }
-
-        sc.close();
     }
 }
