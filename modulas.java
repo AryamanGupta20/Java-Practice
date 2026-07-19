e96d53f8a5de9135af00ad3d84be6c52a9dd0134
aryaman <aryamangupta55@gmail.com>
2026-07-19 16:46:00 +0530

    imp operator

--- modulas.java
+++ modulas.java
@@ -2,6 +2,7 @@ public class modulas {
     public static void main(String[] args) {
         int a = 8;
         int b = 2;
-        System.out.println(a%b);
+        
+     System.out.println(a%b);
     }
 }
