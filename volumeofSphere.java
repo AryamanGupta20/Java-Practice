00ef533ddc1b2202b72bb930dc5fc2a8a8123855
aryaman <aryamangupta55@gmail.com>
2026-07-21 18:05:22 +0530

    volume

--- volumeofSphere.java
+++ volumeofSphere.java
@@ -3,6 +3,6 @@ public class volumeofSphere {
         double r = 5;
         double pi = 3.14;
         double volume = 4*pi*r*r*r/3;
-        System.out.println("volume of spjere is :"+volume);
+    System.out.println("volume of spjere is :"+volume);
     }
 }
