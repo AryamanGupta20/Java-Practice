5ebd479004624caf0ca8af61143ab9365fac3c72
aryaman <aryamangupta55@gmail.com>
2026-07-22 19:20:47 +0530

    5+ while loop problems

--- whileloop.java
+++ whileloop.java
@@ -11,20 +11,128 @@
 // }
 
 
-import java.util.Scanner;          // to count digits of any number
-public class whileloop{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        int n = sc.nextInt();
-        int count = 0;
-        int sum = 0;
+// import java.util.Scanner;          // to count digits of any number
+// public class whileloop{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner (System.in);
+//         int n = sc.nextInt();
+//         int count = 0;
+//         int sum = 0;
 
-        while(n!=0)     // condition
-        {
-           int ld = n % 10;
-           sum+=ld;
-           n /= 10;
+//         while(n!=0)     // condition
+//         {
+//            int ld = n % 10;
+//            sum+=ld;
+//            n /= 10;
             
-        }
-         System.out.println(sum);
-    }}
+//         }
+//          System.out.println(sum);
+//     }}
+
+// import java.util.Scanner;         //print 1 to 10
+// public class whileloop{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         int i = 1;
+
+//         while(i <= 10)
+//         {
+//             System.out.println(i);
+//             i++;
+//         }
+//    sc.close();
+//  }
+// }
+
+
+// import java.util.Scanner;        //print reverse 10 to 1
+// public class whileloop{
+//     public static void main(String[] args) {
+//         int i = 10;
+//         while(i >= 1)
+//         {
+//             System.out.println(i);
+//             i--;
+//         }
+//     }
+// }
+
+
+// public class whileloop{          // print even numbers 1 to 100
+//     public static void main(String[] args) {
+//         int i = 2;
+
+//         while( i <= 100)
+//         {
+//             System.out.println(i);
+//             i += 2;
+//         }
+//     }
+// }
+
+// public class whileloop{             // odd numbers 
+//     public static void main(String[] args) {
+//         int i = 3;
+
+//         while(i <= 100)
+//         {
+//             System.out.println(i);
+//             i += 2;
+//         }
+//     }
+// }
+
+
+// import java.util.Scanner;        //multiplication table
+// public class whileloop{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner (System.in);
+//         System.out.println("enter the number");
+//         int n = sc.nextInt();
+
+//         int i = 1;
+
+//         while(i <= 10)
+//         {
+//             System.out.println(n +"*"+i+"="+n*i);
+//             i++;
+//         }
+//     }
+// }
+
+// import java.util.Scanner;      //sum of even number sfrom 1 to n
+// public class whileloop{
+//     public static void main(String[] args) {
+//          Scanner sc = new Scanner (System.in);
+//          int n = sc.nextInt();
+
+//         int i = 2;
+//          int sum= 0;
+
+//          while(i <= n)
+//          {
+//             sum = sum + i;
+//             System.out.println(i);
+//             i += 2;
+//          }
+//          System.out.println(sum);
+//     }
+// }
+
+// import java.util.Scanner;         // print odd numbers 1 to n
+// public class whileloop{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int i = 3;
+//         int sum = 0;
+
+//         while(i <= n)
+//         {
+//             sum = sum + i;
+//             i += 2;
+//         }
+//         System.out.println(sum);
+//     }
+// }
