38a587b1c72bf957b43a09267b0864e6c8ea6d33
aryaman <aryamangupta55@gmail.com>
2026-07-25 20:08:31 +0530

    pattern intro

---
+++ pattern.java
@@ -0,0 +1,68 @@
+// import java.util.Scanner;            // user input patterns
+
+// public class pattern {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner (System.in);
+//         int m = sc.nextInt();
+//         int n = sc.nextInt();
+
+//         for (int i = 1;i<=m;i++)          // it control no. of rows
+//         {
+//             for(int j =1; j <= n;j++)      // it control colmn
+//                 {
+//                     System.out.print("* ");
+//                 }
+//                 System.out.println();          // to give next line 
+//         }
+//    sc.close();
+//  }
+// }
+
+// public class pattern               // star square
+// {
+//     public static void main(String[] args) {
+        
+//         for(int i = 1; i <= 4 ;i++)
+//         {
+//             for (int j =1;j<=4;j++)
+//             {
+//                 System.out.print("x ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// import java.util.Scanner;
+
+// public class pattern{          //number square
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         for (int i = 1;i<=n;i++)
+//         {
+//             for(int j = 1;j <= n;j++)
+//             {
+//                 System.out.print(j+" ");     // printing j  to print whole counting 1 to n
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+import java.util.Scanner;           // alphabet squre
+public class pattern{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 1;i <= n;i++)
+        {
+            for(char j =1; j <=n;j++)
+            {
+                System.out.print((char)(64+j));   //type casting
+            }
+            System.out.println();
+        }
+    }
+}
