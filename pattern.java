bb32c15be45a604c2c90db87e256084e13cced81
aryaman <aryamangupta55@gmail.com>
2026-07-26 16:51:21 +0530

    5+ patterns

--- pattern.java
+++ pattern.java
@@ -67,19 +67,36 @@
 //     }
 // }
 
-import java.util.Scanner;    //revion
-public class pattern{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        int n = sc.nextInt();
+// import java.util.Scanner;    //revion
+// public class pattern{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner (System.in);
+//         int n = sc.nextInt();
 
-        for(int i = 1;i<=n;i++)
-        {
-            for(int j =1;j<=n;j++)
-            {
-                System.out.print((char)(j+64));
-            }
-        System.out.println();
-    }
-    }
-}
+//         for(int i = 1;i<=n;i++)
+//         {
+//             for(int j =1;j<=n;j++)
+//             {
+//                 System.out.print((char)(j+64));
+//             }
+//         System.out.println();
+//     }
+//     }
+// }
+
+
+// import java.util.Scanner;    //alphabet square capital letter
+// public class pattern{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         for(int i =1; i <= n; i++)
+//         {
+//             for (int j =1; j <=n ;j++)
+//             {
+//                 System.out.print((char)(i+64));
+//             }
+//     System.out.println();    }
+//     }
+// }
