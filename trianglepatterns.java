3b66086ab7ae9220bd3eedb8684dad1188226414
aryaman <aryamangupta55@gmail.com>
2026-07-30 05:05:56 +0530

    9+ patterns

--- trianglepatterns.java
+++ trianglepatterns.java
@@ -322,21 +322,42 @@
 //   }
 // }
 
-import java.util.Scanner;           //method 1
-public class trianglepatterns{
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
+// import java.util.Scanner;           //method 1 star triangle flipped
+// public class trianglepatterns{
+//   public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     int n = sc.nextInt();
 
-    for(int i=1;i<=n;i++)
-    {
-      for(int j=1;j<=n;j++)
-      {
-        if (i+j>n)
-          System.out.print("*"+" ");
-        else System.out.print(" "+" ");
-      }
-    System.out.println();
-  }
-  }
-}
+//     for(int i=1;i<=n;i++)
+//     {
+//       for(int j=1;j<=n;j++)
+//       {
+//         if (i+j>n)
+//           System.out.print("*"+" ");
+//         else System.out.print(" "+" ");
+//       }
+//     System.out.println();
+//   }
+//   }
+// }
+
+// import java.util.Scanner;             // composite important method of star triangle flip
+// public class trianglepatterns{
+//   public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     int n = sc.nextInt();
+
+//     for(int i =1;i<=n;i++)
+//     {
+//       for(int j =1;j<=n-i;j++)
+//       {
+//         System.out.print(" "+" ");
+//       }
+//       for(int j=1;j<=i;j++)
+//       {
+//         System.out.print(j+" ");
+//       }
+//     System.out.println();
+//   }
+//   }
+// }
