1fc11151d20ca87aa8f611824110a2df52c6d8f9
aryaman <aryamangupta55@gmail.com>
2026-08-02 18:54:53 +0530

    17+ patterns

--- trianglepatterns.java
+++ trianglepatterns.java
@@ -678,3 +678,26 @@
 //     }
 // }
 
+// import java.util.Scanner;     //rhombus
+// /**
+//  * trianglepatterns
+//  */
+// public class trianglepatterns {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         for(int i=1;i<=n;i++)
+//         {
+//             for(int j=1;j<=n-i;j++)
+//             {
+//                 System.out.print(" "+" ");
+//             }
+//             for(int j =1;j<=2*n-1;j++)
+//             {
+//                 System.out.print("*"+" ");
+//             }
+//         System.out.println();}
+//     }
+// }
