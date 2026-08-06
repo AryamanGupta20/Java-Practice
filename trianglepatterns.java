96b1d02fbe0ef75f040074feab3c2b2e3282eea9
aryaman <aryamangupta55@gmail.com>
2026-08-06 20:58:24 +0530

    19+ patterns

--- trianglepatterns.java
+++ trianglepatterns.java
@@ -761,37 +761,73 @@
 // }
 // }
 
+// import java.util.Scanner;  triangle+horizontal flip triangle
+/**
+ * trianglepatterns
+ */
+// public class trianglepatterns {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+        
+//     for(int i=1;i<=2*n-1;i++)
+//     {
+//         System.out.print("*"+" ");
+//     }
+//     System.out.println();
+//     n--;
+
+//     for(int i=1;i<=n;i++)
+//     {
+//         for(int j=1;j<=n+1-i;j++)
+//         {
+//             System.out.print("*"+" ");
+//         }
+//         for(int j=1;j<=2*i-1;j++)
+//         {
+//             System.out.print(" "+" ");
+//         }
+//         for(int j=1;j<=n+1-i;j++)
+//         {
+//             System.out.print("*"+" ");
+//         }
+//  System.out.println();   }
+// }
+// }
+
 import java.util.Scanner;
+
 /**
  * trianglepatterns
  */
 public class trianglepatterns {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        
-    for(int i=1;i<=2*n-1;i++)
-    {
-        System.out.print("*"+" ");
+        Scanner sc = new Scanner (System.in);
+        int n= sc.nextInt();
+        for(int i=1;i<=2*n-1;i++)
+        {
+            System.out.print(i);
+        }
+        System.out.println();
+        n--;
+        for(int i=1;i<=n;i++)
+        {
+            int a=1;
+            for(int j=1;j<=n+1-i;j++)
+            {
+                System.out.print(a++);
+            }
+            for(int j=1;j<=2*i-1;j++)
+            {
+                System.out.print(" "+" ");
+                a++;
+            }
+            for(int j=5;j<=n+5-i;j++)
+            {
+                System.out.print(a++);
+            }
+        System.out.println();}
     }
-    System.out.println();
-    n--;
-
-    for(int i=1;i<=n;i++)
-    {
-        for(int j=1;j<=n+1-i;j++)
-        {
-            System.out.print("*"+" ");
-        }
-        for(int j=1;j<=2*i-1;j++)
-        {
-            System.out.print(" "+" ");
-        }
-        for(int j=1;j<=n+1-i;j++)
-        {
-            System.out.print("*"+" ");
-        }
- System.out.println();   }
-}
 }
