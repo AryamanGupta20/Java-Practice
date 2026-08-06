c6a44c7c5d799ecf5441f2d9b4bbdf7d8dad8008
aryaman <aryamangupta55@gmail.com>
2026-08-06 20:33:33 +0530

    18+ patterns

--- trianglepatterns.java
+++ trianglepatterns.java
@@ -731,3 +731,67 @@
 //     }
 // }
 
+// import java.util.Scanner;
+// /**
+//  * trianglepatterns
+//  */
+// public class trianglepatterns {
+// public static void main(String[] args) {
+    
+
+//     Scanner sc = new Scanner(System.in);
+//     int n = sc.nextInt();
+
+//     for(int i=1;i<=n;i++)
+//     {
+//         for(int j=1;j<=n-i;j++)
+//         {
+//         System.out.print(" "+" ");
+//         }
+//         for(int j=1;j<=2*i-1;j++)
+//         {
+//             System.out.print(j);
+//         }
+//         for(int j=i-1;j>=1;j--)
+//         {
+//             System.out.print(j);
+//         }
+//         System.out.println();
+//     }
+// }
+// }
+
+import java.util.Scanner;
+/**
+ * trianglepatterns
+ */
+public class trianglepatterns {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+    for(int i=1;i<=2*n-1;i++)
+    {
+        System.out.print("*"+" ");
+    }
+    System.out.println();
+    n--;
+
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n+1-i;j++)
+        {
+            System.out.print("*"+" ");
+        }
+        for(int j=1;j<=2*i-1;j++)
+        {
+            System.out.print(" "+" ");
+        }
+        for(int j=1;j<=n+1-i;j++)
+        {
+            System.out.print("*"+" ");
+        }
+ System.out.println();   }
+}
+}
