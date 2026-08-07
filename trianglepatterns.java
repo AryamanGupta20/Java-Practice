547fcff9fdcdfb67a57aecef6e83b57ff83e58f1
aryaman <aryamangupta55@gmail.com>
2026-08-07 20:44:47 +0530

    20+ patterns

--- trianglepatterns.java
+++ trianglepatterns.java
@@ -796,38 +796,75 @@
 // }
 // }
 
-import java.util.Scanner;
+// import java.util.Scanner;   //star bridge
 
-/**
- * trianglepatterns
- */
-public class trianglepatterns {
+// /**
+//  * trianglepatterns
+//  */
+// public class trianglepatterns {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        int n= sc.nextInt();
-        for(int i=1;i<=2*n-1;i++)
-        {
-            System.out.print(i);
-        }
-        System.out.println();
-        n--;
-        for(int i=1;i<=n;i++)
-        {
-            int a=1;
-            for(int j=1;j<=n+1-i;j++)
-            {
-                System.out.print(a++);
-            }
-            for(int j=1;j<=2*i-1;j++)
-            {
-                System.out.print(" "+" ");
-                a++;
-            }
-            for(int j=5;j<=n+5-i;j++)
-            {
-                System.out.print(a++);
-            }
-        System.out.println();}
-    }
-}
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner (System.in);
+//         int n= sc.nextInt();
+//         for(int i=1;i<=2*n-1;i++)
+//         {
+//             System.out.print(i);
+//         }
+//         System.out.println();
+//         n--;
+//         for(int i=1;i<=n;i++)
+//         {
+//             int a=1;
+//             for(int j=1;j<=n+1-i;j++)
+//             {
+//                 System.out.print(a++);
+//             }
+//             for(int j=1;j<=2*i-1;j++)
+//             {
+//                 System.out.print(" "+" ");
+//                 a++;
+//             }
+//             for(int j=5;j<=n+5-i;j++)
+//             {
+//                 System.out.print(a++);
+//             }
+//         System.out.println();}
+//     }
+// }
+
+// import java.util.Scanner;
+// /**
+//  * trianglepatterns
+//  */
+// public class trianglepatterns {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner (System.in);
+//         int n = sc.nextInt();
+
+//         for(int i=1;i<=2*n-1;i++)
+//         {
+//             System.out.print(i);
+//         }
+//         System.out.println();
+//         n--;
+//         for(int i=1;i<=n;i++)
+//         {
+//             int a=1;
+//         for(int j =1;j<=n+1-i;j++)
+//         {
+//             System.out.print(a++ +" ");
+//         }
+//         for(int j=1;j<=2*i-1;j++)
+//         {
+//             System.out.print(" "+" ");
+//             a++;
+//         }
+//         for(int j=1;j<=n+1-i;j++)
+//         {
+//             System.out.print(a++ +" ");
+//         }
+//     System.out.println();
+//     }
+// }}
+
